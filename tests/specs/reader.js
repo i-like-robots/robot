bb4315b3e2baf6d92015db546aaede0a26c813e9
Matt Hinchliffe <matt@maketea.co.uk>
2013-04-04 23:18:49 +0100

    Added shared reader module and test specs.

---
+++ tests/specs/reader.js
@@ -0,0 +1,77 @@
+var assert = require('assert');
+var instance = require('../../lib/reader.js');
+
+describe('Reader', function() {
+
+  describe('Scan directory', function() {
+
+    it('Should return an array of files from directory', function(done) {
+      instance.scanDirectory('../mocks/test-layouts/layouts', function(err, files) {
+        assert.ok(files.length);
+        done();
+      });
+    });
+
+    it('Should return an error attempting to scan an incorrect path', function(done) {
+      instance.scanDirectory('', function(err, files) {
+        assert.ok(err instanceof Error);
+        done();
+      });
+    });
+
+  });
+
+  describe('Filter by file type', function() {
+
+    it('Should return an array of the same size if all files match', function() {
+      var input = ['path/to/file.type'];
+      var output = instance.filterByFileType(input, ['.type']);
+      assert.equal(input.length, output.length);
+    });
+
+    it('Should return an empty array when no files match', function() {
+      var input = ['path/to/file'];
+      var output = instance.filterByFileType(input, ['.type']);
+      assert.equal(output.length, 0);
+    });
+
+  });
+
+  describe('Read files', function() {
+
+    it('Should read each file and send data via callback', function(done) {
+      var i = 0;
+      var files = ['../mocks/test-partials/partials/partial.html', '../mocks/test-layouts/layouts/layout.html'];
+
+      instance.readFiles(
+        files,
+        function(file, data) {
+          i++;
+          assert.ok(data.length);
+        },
+        function(err) {
+          assert.equal(i, files.length);
+          done();
+        }
+      );
+    });
+
+    it('Should return an error attempting to read an invalid file', function(done) {
+      var i = 0;
+
+      instance.readFiles(
+        [''],
+        function(file, data) {
+          i++;
+        },
+        function(err) {
+          assert.ok(err instanceof Error);
+          assert.equal(i, 0);
+          done();
+        }
+      );
+    });
+
+  });
+
+});
