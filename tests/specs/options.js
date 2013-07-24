ee3b7aae0d1e1c3670499af6f86ae4a2d59401b7
Matt Hinchliffe <matt@maketea.co.uk>
2013-07-24 17:47:21 +0100

    Implement support for project-level options file and spec option merge.

--- tests/specs/options.js
+++ tests/specs/options.js
@@ -2,20 +2,38 @@ var path = require('path');
 var assert = require('assert');
 var Options = require('../../lib/robot/options.js');
 
-describe('Config', function() {
+describe('Options', function() {
 
-  var mocks = path.join(__dirname, '../mocks');
+  var mocks = path.join(__dirname, '../mocks/options');
 
   describe('Set options', function() {
 
-    it('Should calculate correct source and output paths', function() {
-      var input = require(path.join(mocks, 'options/robot.json'));
-      var instance = new Options(mocks, input);
+    it('Should calculate correct paths based on user given options', function() {
+      var instance = new Options(mocks, {
+        source: 'baz/',
+        output: 'qux/'
+      });
 
-      assert.equal(instance.sourcePath, path.join(mocks, 'source/'));
-      assert.equal(instance.outputPath, path.join(mocks, 'output/'));
+      assert.equal(instance.sourcePath, path.join(mocks, 'baz/'));
+      assert.equal(instance.outputPath, path.join(mocks, 'qux/'));
+    });
+
+    it('Should calculate correct paths based on project options', function() {
+      var input = require(path.join(mocks, 'robot.json'));
+      var instance = new Options(mocks, {});
+
+      assert.equal(instance.sourcePath, path.join(mocks, input.source));
+      assert.equal(instance.outputPath, path.join(mocks, input.output));
     });
 
   });
+
+//  describe('Load project options', function() {
+//
+//    it('Should load the project options file', function() {
+//
+//    });
+//
+//  });
 
 });
